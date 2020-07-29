295d2d756803fe611ff85a984aa9aed7819626a3
Pauline Ribeyre <ribeyre@uchicago.edu>
2020-07-29 11:28:19 -0500

    test that code owners are not requested for review

--- gdcdictionary/__init__.py
+++ gdcdictionary/__init__.py
@@ -4,3 +4,4 @@ from dictionaryutils import DataDictionary as GDCDictionary
 SCHEMA_DIR = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), 'schemas')
 gdcdictionary = GDCDictionary(root_dir=SCHEMA_DIR)
+test
